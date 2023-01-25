63e8b768ae637d721eabc54e42564a5dd6619af3
Madelyn Cruz <mcruz42@calpoly.edu>
2023-01-25 14:01:39 -0800

    first commit

---
+++ portfolio.test.js
@@ -0,0 +1,16 @@
+const myFunctions = require('./portfolio.js');
+
+test('null to check that jest works', () => {
+    const n = null;
+    expect(n).toBeNull();
+    expect(n).toBeDefined();
+    expect(n).not.toBeUndefined();
+    expect(n).not.toBeTruthy();
+    expect(n).toBeFalsy();
+  });
+
+test('Testing createPortfolio -- success', () => {
+  const target = [];
+  const result = myFunctions.createPortfolio();
+  expect(target).toBe(result);
+});
